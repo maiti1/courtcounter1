90048e8bc6b4e2c9aff15794c3069aa78a9db9a0
Admin <fullcmaiti@gmail.com>
2016-09-16 15:47:11 +0200

    changes to the methods of scores

--- app/src/main/java/com/example/admin/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/admin/courtcounter/MainActivity.java
@@ -7,41 +7,56 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    int score = 0;
+    int score1 = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    //call this method for kaizer score
+    //call this method for TeamA score
     public void displayScore(View view)
     {
-        addScore(3);
+        score = score + 3;
+        addScore(score);
     }
     public void display(View view)
     {
-        scoreAdd(2);
+        score = score + 2;
+        scoreAdd(score);
     }
     public void freeDisplay(View view)
     {
-        freeScore(1);
+        score = score + 1;
+        freeScore(score);
     }
 
-    //call this methods for orlando scores
-    public void orlandoDisplay(View view)
+    //call this methods for team2 scores
+    public void teamDisplay(View view)
     {
-        orlandoAdd(3);
+        score1 = score1 + 3;
+        teambAdd(score1);
     }
-    public void orlndDisplay(View view)
+    public void taemBDisplay(View view)
     {
-        orlndAdd(2);
+        score1 = score1 + 2;
+        teamBAdd(score1);
     }
-    public void orlandoFree(View view)
+    public void teambBFree(View view)
     {
-        orlndFreeAdd(1);
+        score1 = score1 + 1;
+        tmBFreeAdd(score1);
     }
 
-    //this methods is for kaizer score
+    //call this method to reset the scores of the teams
+    public void resetScores(View view)
+    {
+        resetMethod();
+    }
+
+    //this methods is for TeamA score
     public void addScore(int number)
     {
         TextView score = (TextView)findViewById(R.id.score_text_view);
@@ -58,21 +73,33 @@ public class MainActivity extends AppCompatActivity {
         freescore.setText(""+number);
     }
 
-    //this methods is for orlando scores
-    public void orlandoAdd(int number)
+    //this methods is for TeamB scores
+    public void teambAdd(int number)
     {
         TextView orlndscore = (TextView)findViewById(R.id.score_text_view2);
         orlndscore.setText(""+number);
     }
-    public void orlndAdd(int number)
+    public void teamBAdd(int number)
     {
         TextView oscore = (TextView)findViewById(R.id.score_text_view2);
         oscore.setText(""+number);
     }
-    public void orlndFreeAdd(int number)
+    public void tmBFreeAdd(int number)
     {
         TextView olndfree = (TextView)findViewById(R.id.score_text_view2);
         olndfree.setText(""+number);
+    }
+
+    //the method to reset the scores to initial value of 0
+    public void resetMethod()
+    {
+        int teamAScore = 0;
+        int teamBScore = 0;
+
+        TextView rescore = (TextView)findViewById(R.id.score_text_view);
+        rescore.setText(""+teamAScore);
+        TextView rescoreo = (TextView)findViewById(R.id.score_text_view2);
+        rescoreo.setText(""+teamBScore);
     }
 
 
